e98720958c4d04fa4cebd435d8a25897e0c7818b
MiyuYokoyama0109 <miyu.y@latona.io>
2022-03-24 12:29:28 +0900

    release

---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	sap_api_caller "sap-api-integrations-warehouse-available-stock-reads-rmq-kube/SAP_API_Caller"
+	sap_api_input_reader "sap-api-integrations-warehouse-available-stock-reads-rmq-kube/SAP_API_Input_Reader"
+	"sap-api-integrations-warehouse-available-stock-reads-rmq-kube/config"
+
+	"github.com/latonaio/golang-logging-library-for-sap/logger"
+	rabbitmq "github.com/latonaio/rabbitmq-golang-client"
+	"golang.org/x/xerrors"
+)
+
+
+func main() {
+	l := logger.NewLogger()
+	conf := config.NewConf()
+	rmq, err := rabbitmq.NewRabbitmqClient(conf.RMQ.URL(), conf.RMQ.QueueFrom(), conf.RMQ.QueueTo())
+	if err != nil {
+		l.Fatal(err.Error())
+	}
+	defer rmq.Close()
+
+	caller := sap_api_caller.NewSAPAPICaller(
+		conf.SAP.BaseURL(),
+		conf.RMQ.QueueTo(),
+		rmq,
+		l,
+	)
+
+	iter, err := rmq.Iterator()
+	if err != nil {
+		l.Fatal(err.Error())
+	}
+	defer rmq.Stop()
+
+	for msg := range iter {
+		err = callProcess(caller, msg)
+		if err != nil {
+			msg.Fail()
+			l.Error(err)
+			continue
+		}
+		msg.Success()
+	}
+}
+
+func callWarehouseAvailableStock(caller *sap_api_caller.SAPAPICaller, msg rabbitmq.RabbitmqMessage) (err error) {
+	defer func() {
+		if e := recover(); e != nil {
+			err = xerrors.Errorf("error occurred: %w", e)
+			return
+		}
+	}()
+	eWMWarehouse, product := extractData(msg.Data())
+	accepter := getAccepter(msg.Data())
+	caller.AsyncGetWarehouseAvailableStock(eWMWarehouse, product, accepter)
+	return nil
+}
+
+func extractData(data map[string]interface{}) (eWMWarehouse, product string) {
+	sdc := sap_api_input_reader.ConvertToSDC(data)
+	eWMWarehouse = sdc.WarehouseAvailableStock.EWMWarehouse
+	product = sdc.WarehouseAvailableStock.Product
+	return
+}
+
+func getAccepter(data map[string]interface{}) []string {
+	sdc := sap_api_input_reader.ConvertToSDC(data)
+	accepter := sdc.Accepter
+	if len(sdc.Accepter) == 0 {
+		accepter = []string{"All"}
+	}
+
+	if accepter[0] == "All" {
+		accepter = []string{
+			"Header",
+		}
+	}
+	return accepter
+}
+
